ee950954225844f88a2f7375fd507148e23ffd7c
Theodor Vulpe <vulpetheodor@gmail.com>
2025-07-01 21:45:45 +0300

    updated metadata and added email contact

--- src/components/Socials.jsx
+++ src/components/Socials.jsx
@@ -1,5 +1,6 @@
 import GithubIcon from '../assets/icons/github.svg';
 import LinkedinIcon from '../assets/icons/linkedin.svg';
+import EmailIcon from '../assets/icons/mail.svg';
 import './Socials.css';
 
 const Socials = () => {
@@ -16,6 +17,11 @@ const Socials = () => {
                     target="_blank"
                     rel="noopener noreferrer">
                     <img src={LinkedinIcon} alt="LinkedIn Icon" />
+                </a>
+                <a href="mailto:vulpetheodor@yahoo.com"
+                    target="_blank"
+                    rel="noopener noreferrer">
+                    <img src ={EmailIcon} alt="Email Icon" />
                 </a>
             </div>
         </div>
